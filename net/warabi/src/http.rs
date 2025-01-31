32226470bf0d1d07f1004f35feae1041a1729557
ikaruga <ikaruga777@gmail.com>
2025-01-31 20:28:35 +0900

    lookup_host という WasabiOS の API を使ってホスト名からIPアドレスへ

--- net/warabi/src/http.rs
+++ net/warabi/src/http.rs
@@ -8,6 +8,9 @@ extern crate alloc;
 use alloc::string::String;
 use saba_core::error::Error;
 use saba_core::http::HttpResponse;
+use alloc::format;
+use crate::alloc::string::ToString;
+use noli::net::lookup_host;
 
 impl HttpClient {
   pub fn new() -> Self {
@@ -15,6 +18,15 @@ impl HttpClient {
   }
 
   pub fn get(&self, host: String, port: u16, path: String) -> Result<HttpResponse,Error> {
-    //あとで
+    let ips = match lookup_host(&host) {
+      Ok(ips) => ips,
+      Err(e) => {
+        return Err(Error::Network(format!("Failed to find IP addresses: {:#?}", e)))
+      }
+    };
+
+    if ips.len() < 1 {
+      return Err(Error::Network("Failed to fin IP addresses".to_string()));
+    }
   }
 }
