433410e4a1c482dbd17aee9b0999d5fd0b2ba456
ikaruga <ikaruga777@gmail.com>
2025-01-31 21:26:39 +0900

    rename

--- saba_core/src/error.rs
+++ saba_core/src/error.rs
@@ -1,10 +1,4 @@
-#![no_std]
-
 extern crate alloc;
-
-pub mod error;
-pub mod http;
-pub mod url;
 
 use alloc::string::String;
 
--- saba_core/src/lib.rs
+++ saba_core/src/lib.rs
@@ -1,5 +1,8 @@
 #![no_std]
 
+extern crate alloc;
+
+pub mod error;
 pub mod url;
 pub mod http;
 pub use url::Url;
