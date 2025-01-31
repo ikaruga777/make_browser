52076fa7aa35480bd92a24a70ba01877a5c7ab73
ikaruga <ikaruga777@gmail.com>
2025-01-31 20:40:37 +0900

    レスポンスを受けますわよ

--- net/warabi/src/http.rs
+++ net/warabi/src/http.rs
@@ -6,6 +6,7 @@ pub struct HttpClient {}
 
 extern crate alloc;
 use alloc::string::String;
+use alloc::vec::Vec;
 use saba_core::error::Error;
 use saba_core::http::HttpResponse;
 use alloc::format;
@@ -61,5 +62,24 @@ impl HttpClient {
         ))
       }
     };
+
+    let mut received = Vec::new();
+    loop {
+      let mut bug = [0u8; 4096];
+      let bytes_read = match stream.read(&mut buf) {
+        Ok(bytes) => bytes,
+        Err(_) => {
+          return Err(Error::Network(
+            "Failed to receive ad request from TCP stream".to_string(),
+          ))
+        }
+      };
+
+      if bytes_read == 0 {
+        break;
+      }
+
+      received.extend_from_slice(&buf[..bytes_read]);
+    }
   }
 }
